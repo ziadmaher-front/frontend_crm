83e86159f7166a27b68a1a2f7f3afc02c96cc94c
ziadmaher-front <z.maheerr2@gmail.com>
2025-11-25 17:26:54 +0200

    Update worker script to use ES modules format

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,20 +1,19 @@
-// Cloudflare Workers script to serve static files
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  
-  // Get the pathname, default to index.html
-  let pathname = url.pathname === '/' ? '/index.html' : url.pathname
-  
-  // Try to get the file from the site bucket
-  try {
-    return await env.ASSETS.fetch(new Request(url.toString(), request))
-  } catch (e) {
-    // If file not found, serve index.html for SPA routing
-    return await env.ASSETS.fetch(new Request(new URL('/index.html', url.origin).toString(), request))
+// Cloudflare Workers script to serve static files for React SPA
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url)
+    
+    // Get the pathname, default to index.html
+    let pathname = url.pathname === '/' ? '/index.html' : url.pathname
+    
+    // Try to get the file from the site bucket
+    try {
+      return await env.ASSETS.fetch(new Request(url.toString(), request))
+    } catch (e) {
+      // If file not found, serve index.html for SPA routing
+      const indexUrl = new URL('/index.html', url.origin)
+      return await env.ASSETS.fetch(new Request(indexUrl.toString(), request))
+    }
   }
 }
 
