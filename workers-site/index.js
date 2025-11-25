2b5aa08ada7654eeea7173f67f981b3392815284
ziadmaher-front <z.maheerr2@gmail.com>
2025-11-25 17:42:42 +0200

    Remove worker script, use Cloudflare automatic static asset serving

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,9 +1,8 @@
 // Cloudflare Workers script to serve static files for React SPA
+// For Cloudflare Workers with [site] configuration, assets are automatically bound
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url)
-    
-    // Get the pathname
     let pathname = url.pathname
     
     // Default to index.html for root
@@ -11,24 +10,28 @@ export default {
       pathname = '/index.html'
     }
     
-    // Try to get the file from assets
+    // For [site] configuration, use the default asset handler
+    // The assets are automatically available through the site binding
     try {
-      // Use the default asset handler
-      const response = await env.ASSETS.fetch(new Request(new URL(pathname, url.origin).toString(), request))
+      // Try to fetch the requested file
+      const assetRequest = new Request(new URL(pathname, url.origin).toString(), request)
+      const response = await env.ASSETS.fetch(assetRequest)
       
       // If file not found (404), serve index.html for SPA routing
       if (response.status === 404 && pathname !== '/index.html') {
-        return await env.ASSETS.fetch(new Request(new URL('/index.html', url.origin).toString(), request))
+        const indexRequest = new Request(new URL('/index.html', url.origin).toString(), request)
+        return await env.ASSETS.fetch(indexRequest)
       }
       
       return response
     } catch (e) {
-      // If error, try to serve index.html
+      // If error accessing assets, try to serve index.html
       try {
-        return await env.ASSETS.fetch(new Request(new URL('/index.html', url.origin).toString(), request))
+        const indexRequest = new Request(new URL('/index.html', url.origin).toString(), request)
+        return await env.ASSETS.fetch(indexRequest)
       } catch (e2) {
         // If index.html also fails, return error
-        return new Response('Not Found', { status: 404 })
+        return new Response('Not Found', { status: 404, statusText: 'File not found' })
       }
     }
   }
