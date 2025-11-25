37eba7ace528aa54fab314963b22cbf13eed5ecc
ziadmaher-front <z.maheerr2@gmail.com>
2025-11-25 17:40:05 +0200

    Fix Cloudflare Workers assets configuration

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,18 +1,35 @@
 // Cloudflare Workers script to serve static files for React SPA
 export default {
-  async fetch(request, env) {
+  async fetch(request, env, ctx) {
     const url = new URL(request.url)
     
-    // Get the pathname, default to index.html
-    let pathname = url.pathname === '/' ? '/index.html' : url.pathname
+    // Get the pathname
+    let pathname = url.pathname
     
-    // Try to get the file from the site bucket
+    // Default to index.html for root
+    if (pathname === '/') {
+      pathname = '/index.html'
+    }
+    
+    // Try to get the file from assets
     try {
-      return await env.ASSETS.fetch(new Request(url.toString(), request))
+      // Use the default asset handler
+      const response = await env.ASSETS.fetch(new Request(new URL(pathname, url.origin).toString(), request))
+      
+      // If file not found (404), serve index.html for SPA routing
+      if (response.status === 404 && pathname !== '/index.html') {
+        return await env.ASSETS.fetch(new Request(new URL('/index.html', url.origin).toString(), request))
+      }
+      
+      return response
     } catch (e) {
-      // If file not found, serve index.html for SPA routing
-      const indexUrl = new URL('/index.html', url.origin)
-      return await env.ASSETS.fetch(new Request(indexUrl.toString(), request))
+      // If error, try to serve index.html
+      try {
+        return await env.ASSETS.fetch(new Request(new URL('/index.html', url.origin).toString(), request))
+      } catch (e2) {
+        // If index.html also fails, return error
+        return new Response('Not Found', { status: 404 })
+      }
     }
   }
 }
